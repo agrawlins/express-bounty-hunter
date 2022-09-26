7833f965a79fbea63041d97b432490302b7f0648
Adam Rawlins <agrawlins@gmail.com>
2022-09-26 02:15:05 -0400

    Completed w/ MongoDB Database and Mongoose. Fully functional and full-stack!

--- routes/bountyRouter.js
+++ routes/bountyRouter.js
@@ -1,77 +1,152 @@
 const express = require('express')
 const bountyRouter = express.Router()
-const {v4: uuid4} = require("uuid")
+const Bounty = require('../models/bounty.js')
 
-const bounties = [
-    {
-        firstName: "Anakin",
-        lastName: "Skywalker",
-        living: "ALIVE",
-        bounty: 150,
-        type: "SITH",
-        _id: uuid4()
-    },
-    {
-        firstName: "Plo",
-        lastName: "Koon",
-        living: "DEAD",
-        bounty: 100,
-        type: "JEDI",
-        _id: uuid4()
-    },
-    {
-        firstName: "Jar-Jar",
-        lastName: "Binks",
-        living: "ALIVE",
-        bounty: 50,
-        type: "SITH",
-        _id: uuid4()
-    },
-    {
-        firstName: "Ahsoka",
-        lastName: "Tano",
-        living: "ALIVE",
-        bounty: 250,
-        type: "JEDI",
-        _id: uuid4()
-    },
-    {
-        firstName: "Obi-Wan",
-        lastName: "Kenobi",
-        living: "ALIVE",
-        bounty: 500,
-        type: "JEDI",
-        _id: uuid4()
-    }
-]
-
-//ROUTES
-bountyRouter.get("/", (req, res) => {
-        res.send(bounties)
+//POST ONE
+bountyRouter.post("/", (req, res, next) => {
+    const newBounty = new Bounty(req.body)
+    newBounty.save((err, savedBounty) => {
+        if(err){
+            res.status(500)
+            return next(err)
+        }
+        return res.status(201).send(savedBounty)
     })
-
-bountyRouter.post("/", (req, res) => {
-    const newBounty = req.body
-    newBounty._id = uuid4()
-    console.log(newBounty)
-    bounties.push(newBounty)
-    res.send(`Successfully added ${newBounty.firstName} ${newBounty.lastName} to the hitlist!`)
 })
 
+//GET ALL
+bountyRouter.get("/", (req, res, next) => {
+    Bounty.find((err, bounties) => {
+        if(err){
+            res.status(500)
+            return next(err)
+        }
+        return res.status(200).send(bounties)
+    })
+})
+
+//GET ONE
+bountyRouter.get("/:bountyId", (req, res, next) => {
+    const bountyId = req.params.bountyId
+    const foundBounty = bounties.find(bounty => bounty._id === bountyId)
+    if(!foundBounty){
+        const error = new Error(`'${bountyId}' was not found`)
+        res.status(500)
+        return next(error)
+    }
+    res.status(200).send(foundBounty)
+})
+
+//GET BY TYPE
+bountyRouter.get("/search/type", (req, res, next) => {
+    Bounty.find({type: req.query.type}, (err, bounties) => {
+        if(err){
+            res.status(500)
+            return next(err)
+        }
+        return res.status(200).send(bounties)
+    })
+})
+
+//PUT ONE
 bountyRouter.put("/:bountyId", (req, res) => {
-    const bountyId = req.params.bountyId
-    const updateObject = req.body
-    const bountyIndex = bounties.findIndex(bounty => bounty._id === bountyId)
-    const updatedBounty = Object.assign(bounties[bountyIndex], updateObject)
-    res.send(updatedBounty)
+    Bounty.findOneAndUpdate(
+        {_id: req.params.bountyId},
+        req.body,
+        {new: true},
+        (err, updatedBounty) => {
+        if(err){
+            res.status(500)
+            return next(err)
+        }
+        return res.status(200).send(`${updatedBounty}`)
+    })
 })
 
+//DELETE ONE
 bountyRouter.delete("/:bountyId", (req, res) => {
-    const bountyId = req.params.bountyId
-    const bountyIndex = bounties.findIndex(bounty => bounty._id === bountyId)
-    bounties.splice(bountyIndex, 1)
-    res.send(`Deleted target from the hitlist`)
+    Bounty.findOneAndDelete(
+        {_id: req.params.bountyId},
+        (err, deletedItem) => {
+        if(err){
+            res.status(500)
+            return next(err)
+        }
+        return res.status(200).send(`Successfully Deleted ${deletedItem.title} from the Database!`)
+    })
 })
+
+// //OLD DATA
+// const bounties = [
+//     {
+//         firstName: "Anakin",
+//         lastName: "Skywalker",
+//         living: "ALIVE",
+//         bounty: 150,
+//         type: "SITH",
+//         _id: uuid4()
+//     },
+//     {
+//         firstName: "Plo",
+//         lastName: "Koon",
+//         living: "DEAD",
+//         bounty: 100,
+//         type: "JEDI",
+//         _id: uuid4()
+//     },
+//     {
+//         firstName: "Jar-Jar",
+//         lastName: "Binks",
+//         living: "ALIVE",
+//         bounty: 50,
+//         type: "SITH",
+//         _id: uuid4()
+//     },
+//     {
+//         firstName: "Ahsoka",
+//         lastName: "Tano",
+//         living: "ALIVE",
+//         bounty: 250,
+//         type: "JEDI",
+//         _id: uuid4()
+//     },
+//     {
+//         firstName: "Obi-Wan",
+//         lastName: "Kenobi",
+//         living: "ALIVE",
+//         bounty: 500,
+//         type: "JEDI",
+//         _id: uuid4()
+//     }
+// ]
+
+// //OLD ROUTES
+// bountyRouter.get("/", (req, res) => {
+//         res.send(bounties)
+//     })
+
+// bountyRouter.post("/", (req, res) => {
+//     const newBounty = req.body
+//     newBounty._id = uuid4()
+//     console.log(newBounty)
+//     bounties.push(newBounty)
+//     res.send(`Successfully added ${newBounty.firstName} ${newBounty.lastName} to the hitlist!`)
+// })
+
+// bountyRouter.put("/:bountyId", (req, res) => {
+//     const bountyId = req.params.bountyId
+//     const updateObject = req.body
+//     const bountyIndex = bounties.findIndex(bounty => bounty._id === bountyId)
+//     const updatedBounty = Object.assign(bounties[bountyIndex], updateObject)
+//     res.send(updatedBounty)
+// })
+
+// bountyRouter.delete("/:bountyId", (req, res) => {
+//     const bountyId = req.params.bountyId
+//     const bountyIndex = bounties.findIndex(bounty => bounty._id === bountyId)
+//     bounties.splice(bountyIndex, 1)
+//     res.send(`Deleted target from the hitlist`)
+// })
 
 //GET
 
--- server.js
+++ server.js
@@ -1,17 +1,29 @@
-const express = require("express")
+const express = require('express')
 const app = express()
-const morgan = require("morgan")
-
-//req = request, res = response
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+const DATABASE_URL = require('dotenv')
 
 //Middleware
-app.use(express.json()) //Looks for a request body, and turns it into 'req.body'
+app.use(express.json())
 app.use(morgan('dev'))
 
+//Connect to Database
+mongoose.connect(
+    "mongodb+srv://MerrimanLyon:13qeadzc%21%23QEADZC@cluster0.xqosq.mongodb.net/bountyhunterdb?retryWrites=true&w=majority",
+    () => console.log("Connected to the Database")
+)
 
 //ROUTES
 app.use("/bounties", require("./routes/bountyRouter.js"))
 
+//Error Handler
+app.use((err, req,res, next) => {
+    console.log(err)
+    return res.send({errMsg: err.message})
+})
+
+//Listener
 app.listen(4000, () => {
-    console.log("The server is running on Port 4000")
+    console.log("The Server is running on Port 4000")
 })
